bd0631a86c475f78f10eff8bf457dd67e9762641
adey-px <aosteph@gmail.com>
2023-03-08 21:33:29 -0600

    Updates

--- article/views.py
+++ article/views.py
@@ -44,23 +44,24 @@ def articles_list(request):
     Get all articles from db,
     last in first out, in pages.
     """
-    articles = Article.objects.all()[::-1]
-    paginator = Paginator(articles, 3)
-    page = request.GET.get("page")
+    return HttpResponse('Hello from here...')
 
-    try:
-        pagination = paginator.page(page)
+    # articles = Article.objects.all()[::-1]
+    # paginator = Paginator(articles, 3)
+    # page = request.GET.get("page")
 
-    except PageNotAnInteger:
-        pagination = paginator.page(1)
+    # try:
+    #     pagination = paginator.page(page)
 
-    except EmptyPage:
-        pagination = paginator.page(paginator.num_pages)
+    # except PageNotAnInteger:
+    #     pagination = paginator.page(1)
 
-    return render(
-        request, "article/articles-list.html", {"articles": pagination, "page": page}
-    )
+    # except EmptyPage:
+    #     pagination = paginator.page(paginator.num_pages)
 
+    # return render(
+    #     request, "article/articles-list.html", {"articles": pagination, "page": page}
+    # )
 
 
 # Article detail
