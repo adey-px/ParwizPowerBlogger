fa9c06210fa3bcd0b79882d1bae682761617d20e
adey-px <aosteph@gmail.com>
2023-06-05 23:27:16 -0500

    Fixed bug in virtual env

--- ParwizPowerBlogger/wsgi.py
+++ ParwizPowerBlogger/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ParwizPowerBlog project.
+WSGI config for ParwizPowerBlogger project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ParwizPowerBlog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ParwizPowerBlogger.settings")
 
 application = get_wsgi_application()
